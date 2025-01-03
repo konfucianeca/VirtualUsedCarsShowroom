a8ba149cb4d2535e470cbf7398d314a8de62b2b9
konfucianeca <vladogg56@gmail.com>
2025-01-03 18:00:06 +0200

    Create applicaton extensions

---
+++ VirtualUsedCarsShowroom/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VirtualUsedCarsShowroom.Data;
+
+namespace VirtualUsedCarsShowroom.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services,IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+        
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options
+                    .SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
